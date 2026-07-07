9dc4e59f220effc2ad8653c14f6d49e5a0581272
hendrowunga <hendrowunga073@gmail.com>
2026-07-08 02:47:40 +0700

    add obj

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3862dcc73f17dbe34119e68fbaee44d9e5cded4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cdd301d6fdd5105971e3d2c05d3b40e2da3d610")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
