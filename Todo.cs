6cdd301d6fdd5105971e3d2c05d3b40e2da3d610
hendrowunga <hendrowunga073@gmail.com>
2026-07-08 02:36:47 +0700

    Add fitur baru

--- Todo.cs
+++ Todo.cs
@@ -1,6 +1,15 @@
-class Todo
+// class Todo
+// {
+//     public int Id { get; set; }
+//     public string? Name { get; set; }
+//     public bool IsComplete { get; set; }
+// }
+
+
+public class Todo
 {
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+    public string? Secret { get; set; }
 }
