b3862dcc73f17dbe34119e68fbaee44d9e5cded4
hendrowunga <hendrowunga073@gmail.com>
2026-07-08 02:31:58 +0700

    Add fitur baru

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bafc4fa95dad826a28b63b06428542aa96a00bbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+253dc28f230ef03b78563f8eb84c87052bb000e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
